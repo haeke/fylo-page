6bb53931b205ef861e69206e819fd05ab436b3ac
haeke <riverae2@student.wpunj.edu>
2019-05-20 14:39:29 -0400

    Updates

---
+++ precache-manifest.fbe4a3fee1f5081b92140266c05859f6.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e233111a9437b4f6d6cf3a32c7a803ad",
+    "url": "/fylo-page/index.html"
+  },
+  {
+    "revision": "fe368e6fa9e11c5db0bf",
+    "url": "/fylo-page/static/css/main.9c93aebc.chunk.css"
+  },
+  {
+    "revision": "5f921cfab5724c10e677",
+    "url": "/fylo-page/static/js/2.1011bc2e.chunk.js"
+  },
+  {
+    "revision": "fe368e6fa9e11c5db0bf",
+    "url": "/fylo-page/static/js/main.aaa696b7.chunk.js"
+  },
+  {
+    "revision": "938de353270c1475538e",
+    "url": "/fylo-page/static/js/runtime~main.9557a146.js"
+  },
+  {
+    "revision": "5f4b8b3e0d56ac7578b1468584142faf",
+    "url": "/fylo-page/static/media/icon-access-anywhere.5f4b8b3e.svg"
+  },
+  {
+    "revision": "aedb3ae079501c43082669d8d427bf6b",
+    "url": "/fylo-page/static/media/icon-any-file.aedb3ae0.svg"
+  },
+  {
+    "revision": "794c5a484aefc6409c3a4e7aceb10eee",
+    "url": "/fylo-page/static/media/icon-arrow.794c5a48.svg"
+  },
+  {
+    "revision": "ca8c71cfbd876e461e850fcdcf9c9b29",
+    "url": "/fylo-page/static/media/icon-collaboration.ca8c71cf.svg"
+  },
+  {
+    "revision": "f558537850876022cbf1d99e525724f4",
+    "url": "/fylo-page/static/media/icon-security.f5585378.svg"
+  },
+  {
+    "revision": "526544ae64c3d7a9cf06bc566728d2b9",
+    "url": "/fylo-page/static/media/illustration-intro.526544ae.png"
+  },
+  {
+    "revision": "99294b1f2757d664921ec643a982959a",
+    "url": "/fylo-page/static/media/illustration-stay-productive.99294b1f.png"
+  },
+  {
+    "revision": "c71bf7bf6985f3ac03fa893fb45bed38",
+    "url": "/fylo-page/static/media/logo.c71bf7bf.svg"
+  }
+]);
